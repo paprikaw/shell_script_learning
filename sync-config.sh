8177b6bb2d0d42f7b4a7b1f0687614c9d4505870
Eric Bai <baixu.must@gmail.com>
2021-01-03 16:22:27 +1100

    sync-config.sh: Add a script to syncronize the config file for v2ray

--- sync-config.sh
+++ sync-config.sh
@@ -9,6 +9,9 @@ while getopts "cs" opt; do
 	c)
 		echo ">>>>>>>>>Copy files to the cliend end..."
 		cp ${V2RAY_CONFIG_LOC}/client_end/config.json /usr/local/etc/v2ray
+		echo ">>>>>>>>>Restart client v2ray..."
+		brew services restart v2ray 
+		brew services 
 		;;
 	s) # Sync for server side
 		echo ">>>>>>>>>Rsync file to the remote server end..."
@@ -16,7 +19,7 @@ while getopts "cs" opt; do
 			echo "V2RAY_CONFIG_LOC and REMOTE_SSH should be set"
 			exit 1
 		else
-			rsync -Pz ${V2RAY_CONFIG_LOC}/server_end/config.json $REMOTE_SSH
+			rsync -Pz ${V2RAY_CONFIG_LOC}/server_end/config.json $REMOTE_SSH:/usr/local/etc/v2ray/config.json
 			echo ">>>>>>>>>Restart v2ray for remote server..."
 			ssh ${REMOTE_SSH} "sudo service v2ray restart; sudo service v2ray status"
 		fi
